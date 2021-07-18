885d73e1ff7733c2af7f7b1eec2a1363ce582366
Julian-Alcaraz <62039908+Julian-Alcaraz@users.noreply.github.com>
2021-07-18 20:33:32 -0300

    Agregamos page home

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:schollinorder/pages/home.dart';
 
 void main() {
-  runApp(App());
-}
-
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.pink.shade100,
-        appBar: AppBar(
-          title: Text("Prueba 1"),
-        ),
-        body: Container(
-          child: Text(
-            "Hola Mundo",
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(MaterialApp(
+    home: Myhome(),
+  ));
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:schollinorder/main.dart';
+import 'package:schollinorder/pages/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(App());
+    await tester.pumpWidget(MaterialApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
