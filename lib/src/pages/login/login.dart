7008a91630ada71328ba18b03e3887f9165ae4f4
nicolgomez <nicol.gomez.epet20@gmail.com>
2021-08-18 20:24:31 -0300

    Finalicé AppBar y cree el container principal en la pagina de Login

--- lib/src/pages/login/login.dart
+++ lib/src/pages/login/login.dart
@@ -1,14 +1,26 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo.shade100,
       appBar: AppBar(
-        title: Text('namePage'),
+        backgroundColor: Color(0xFF364562),
+        title: Text('Inicio de sesión'),
+        centerTitle: true,
+        elevation: 8,
       ),
       body: Center(
-        child: Text('namePage'),
+        child: Container(
+            width: 400,
+            height: 400,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                border: Border.all(color: Colors.black))),
       ),
     );
   }
