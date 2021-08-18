36a65e33f6365a9132f9763ce466200ad1b1ccdb
Camila Garces <garces.camila.epet20@gmail.com>
2021-08-18 20:20:02 -0300

    Avance primeros dos botones terminados pagina admin

--- lib/src/pages/administrativos/administrativos.dart
+++ lib/src/pages/administrativos/administrativos.dart
@@ -5,16 +5,73 @@ class AdministrativosPages extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Administrativos"),
-        backgroundColor: Colors.black,
+        backgroundColor: Color(0xFF364562),
+        leading: Container(
+          width: 50,
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: Image.asset(
+              "assets/logo.jpg",
+            ),
+          ),
+        ),
       ),
       body: Center(
-        child: Text(
-          "hola admin",
-          style: TextStyle(
-            color: Colors.blue,
-            fontSize: 40,
-          ),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 50, horizontal: 25),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    primary: Color(0xFF364562), minimumSize: Size(200, 95)),
+                child: Text(
+                  "Cargar datos \nde profesor ",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: (20),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    primary: Color(0xFF364562), minimumSize: Size(200, 95)),
+                child: Text(
+                  "Ver datos \nde profesor ",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: (20),
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(primary: Color(0xFF364562)),
+              child: Text(
+                "Cargar datos\nde profesores ",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: (20),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(primary: Color(0xFF364562)),
+              child: Text(
+                "Modificar \nhorario de curso ",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: (20),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
