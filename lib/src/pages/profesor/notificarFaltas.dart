75e8eb09b06b103f1c38dd47408f2d63281f4c62
matiascubitto <cubitto.matias.epet20@gmail.com>
2021-08-19 12:37:58 -0300

    cree pagina cargar faltas

---
+++ lib/src/pages/profesor/notificarFaltas.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CargarFaltasPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('namePage'),
+      ),
+      body: Center(
+        child: Text('namePage'),
+      ),
+    );
+  }
+}
