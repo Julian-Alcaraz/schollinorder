84160e300016cd278dca5609f4d5535eaf166082
nicolgomez <nicol.gomez.epet20@gmail.com>
2021-08-19 11:40:13 -0300

    creo pagina crear cuenta

---
+++ lib/src/pages/login/crearcuenta.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CrearCuenta extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('namePage'),
+      ),
+      body: Center(
+        child: Text('namePage'),
+      ),
+    );
+  }
+}
--- lib/src/pages/login/login.dart
+++ lib/src/pages/login/login.dart
@@ -15,13 +15,58 @@ class LoginPage extends StatelessWidget {
       ),
       body: Center(
         child: Container(
-            width: 400,
-            height: 400,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(40.0)),
-                border: Border.all(color: Colors.black))),
+          width: 400,
+          height: 400,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(40.0)),
+              border: Border.all(color: Colors.black)),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                campousuario(),
+                campocontrasena(),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Iniciar Sesion'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF364562),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('Crear Cuenta'),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
+}
+
+Widget campousuario() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+    child: TextField(
+        decoration: InputDecoration(
+      hintText: "Usuario/Email",
+    )),
+  );
+}
+
+Widget campocontrasena() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+    child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          hintText: "Contraseña",
+        )),
+  );
 }
