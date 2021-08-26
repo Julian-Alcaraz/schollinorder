466333b00c34b2a36e13f253d907471335c08645
matiascubitto <cubitto.matias.epet20@gmail.com>
2021-08-26 15:36:20 -0300

    empezando con el notificar falta

--- lib/src/pages/profesor/notificarFaltas.dart
+++ lib/src/pages/profesor/notificarFaltas.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
 class CargarFaltasPage extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('namePage'),
-      ),
-      body: Center(
-        child: Text('namePage'),
-      ),
+     key: _scaffoldKey,
+      endDrawer: Drawer(
+          child: Column(
+        children: [
+          DrawerHeader(child: Text("Notificador de Faltas")),
+          Spacer(),
+          ListTile(
+            tileColor: Color(0xFF364562),
+            title: Text("Cerrar Sesion"),
+            onTap: () {},
+          ),
+        ],
+      )),
+     
     );
   }
 }
