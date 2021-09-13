9aaa68e775ff3e05d42ac632aa27efa07af9fcb5
Julian Alcaraz <julianalcaraz4@gmail.com>
2021-09-12 23:31:18 -0300

    acomodo errores del merge

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:schollinorder/src/pages/administrativos/cargarDatosProfesor.dart';
+import 'package:schollinorder/src/pages/administrativos/cargarFaltasProfesor.dart';
+import 'package:schollinorder/src/pages/administrativos/modificarHorarioCurso.dart';
+import 'package:schollinorder/src/pages/administrativos/verDatosProfesor.dart';
 import 'package:schollinorder/src/pages/home/home.dart';
 import 'package:schollinorder/src/pages/alumno/alumno.dart';
 import 'package:schollinorder/src/pages/login/crearcuenta.dart';
 import 'package:schollinorder/src/pages/login/login.dart';
-import 'package:schollinorder/src/pages/profesor/notificarFaltas.dart';
 import 'package:schollinorder/src/pages/profesor/profesor.dart';
 import 'package:schollinorder/src/pages/administrativos/administrativos.dart';
 import 'package:schollinorder/src/pages/splash/splash.dart';
@@ -16,6 +19,10 @@ class MyRoutes {
         "/Profesor": (_) => ProfesorPage(),
         "/Administrativos": (_) => AdministrativosPages(),
         "/Login": (_) => LoginPage(),
-        "/CargarFaltasPage": (_) => CargarFaltasPage(),
+        "/CrearCuenta": (_) => CrearCuenta(),
+        "/CargarDatos": (_) => CargarDatosProfesor(),
+        "/VerDatos": (_) => VerDatosProfesor(),
+        "/CargarFaltas": (_) => CargarFaltasProfesor(),
+        "/ModificarHorario": (_) => ModificarHorarioCurso(),
       };
 }
