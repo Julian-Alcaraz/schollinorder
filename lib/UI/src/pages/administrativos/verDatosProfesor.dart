05f4cf846b260be9af51dd6bbb403a5b19ae1d85
Julian Alcaraz <julianalcaraz4@gmail.com>
2021-09-27 21:10:59 -0300

    appbar y drawer

--- lib/UI/src/pages/administrativos/verDatosProfesor.dart
+++ lib/UI/src/pages/administrativos/verDatosProfesor.dart
@@ -1,14 +1,75 @@
 import 'package:flutter/material.dart';
 
 class VerDatosProfesor extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      endDrawer: drawer(context),
       appBar: AppBar(
-        title: Text("New page"),
+        backgroundColor: Color(0xFF364562),
+        leading: Container(
+          width: 50,
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: Image.asset(
+              "assets/logo.jpg",
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.account_circle_rounded,
+              size: 35,
+            ),
+            onPressed: () {
+              _scaffoldKey.currentState.openEndDrawer();
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Text("New page"),
+      ),
+    );
+  }
+
+  Widget drawer(context) {
+    return Drawer(
+      child: Material(
+        color: Colors.indigo.shade100,
+        child: Column(
+          children: [
+            DrawerHeader(
+              child: Center(
+                child: Column(
+                  children: [
+                    CircleAvatar(maxRadius: 60),
+                    Text("Nombre Administrador"),
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              focusColor: Colors.amber,
+              title: Text("Notificaciones"),
+              tileColor: Colors.indigo.shade300,
+              onTap: () {},
+            ),
+            Spacer(),
+            ListTile(
+              focusColor: Colors.amber,
+              title: Text("Cerrar Sesion"),
+              tileColor: Color(0xFF364562),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed("/Login");
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
