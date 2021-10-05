286b6ac00853fa3f58d05ad009725990538f70d6
Julian-Alcaraz <julianalcaraz4@gmail.com>
2021-10-05 15:50:09 -0300

    commit arreglo el apellido de widy

--- lib/UI/src/models/profesor.dart
+++ lib/UI/src/models/profesor.dart
@@ -8,9 +8,9 @@ class Profesor {
 }
 
 final profesor = Profesor(
-    apellido: "Wiederman", nombre: "Exequiel", numero: "150", dni: "40501");
+    apellido: "Wiedermann", nombre: "Exequiel", numero: "150", dni: "40501");
 final profesor1 = Profesor(
-    apellido: "Wiederman", nombre: "Exequiel", numero: "55", dni: "266156");
+    apellido: "Wiedermann", nombre: "Exequiel", numero: "55", dni: "266156");
 final profesor2 = Profesor(
     apellido: "Bottaso", nombre: "Laura", numero: "54548", dni: "1661");
 final profesor3 = Profesor(
@@ -18,13 +18,13 @@ final profesor3 = Profesor(
 final profesor4 =
     Profesor(apellido: "Gabis", nombre: "Matias", numero: "245", dni: "15151");
 final profesor5 = Profesor(
-    apellido: "Wiederman", nombre: "Exequiel", numero: "1234", dni: "0251");
+    apellido: "Wiedermann", nombre: "Exequiel", numero: "1234", dni: "0251");
 //PROFESOR TALLER
 final profesor6 = Profesor(
     apellido: "Baier", nombre: "Alejandro", numero: "06151", dni: "21516");
 final List<Profesor> listaProfesores = [
   Profesor(
-      apellido: "Wiederman", nombre: "Exequiel", numero: "1234", dni: "0251"),
+      apellido: "Wiedermann", nombre: "Exequiel", numero: "1234", dni: "0251"),
   Profesor(apellido: "Bottaso", nombre: "Laura", numero: "54548", dni: "1661"),
   Profesor(
       apellido: "Calderon", nombre: "Erik", numero: "1254", dni: "2105616"),
