6543eb8fc51fecffcd1ec7d99ad57f5c5110b29e
Julian Alcaraz <julianalcaraz4@gmail.com>
2021-10-06 11:35:14 -0300

    pruebas sobre patron bloc

--- lib/BLoc/Events/events.dart
+++ lib/BLoc/Events/events.dart
@@ -1,5 +1,6 @@
 //importar pagina genera eventoç
 // login al iniciar sesion
+//--------------------- al inicar sesion se dispara este evento-----------------
 class PrincipalEvent {
   String tipoCuenta;
   String contrasenia;
@@ -7,41 +8,38 @@ class PrincipalEvent {
   void tipoDeCuenta() {
     switch (tipoCuenta) {
       case 'alumno':
-        {
-          AlumnoEvent(/* contrasenia, correoElectronico */);
-        }
+        AlumnoEvent(/* contrasenia, correoElectronico */);
         break;
       case 'profesor':
-        {
-          ProfesorEvent(/* contrasenia, correoElectronico */);
-        }
+        ProfesorEvent(/* contrasenia, correoElectronico */);
         break;
 
       case 'administradores':
-        {
-          AdministradoresEvent(/* contrasenia, correoElectronico */);
-        }
+        AdministradoresEvent(/* contrasenia, correoElectronico */);
         break;
 
       default:
-        {
-          //statment
-        }
+      //statment
+
     }
   }
 }
 
+//-------------dependiendo el tipo de cuenta se disapar un estado-------------
 class AlumnoEvent {}
 
 class AdministradoresEvent {}
 
 class ProfesorEvent {}
 
+//--------- una vez sabemos que tipo de  cuenta es ejecutamos el evento correspondiente---------------------
+// eventos de cargaod exitoso de las 3 variables
 class LoadedAlumnoEvent extends AlumnoEvent {}
 
 class LoadedAdminEvent extends AdministradoresEvent {}
 
 class LoadedProfesorEvent extends ProfesorEvent {}
+// evento de no cargado de las 3 variables
 
 class DenegadoAlumnoEvent extends AlumnoEvent {}
 
@@ -49,15 +47,19 @@ class DenegadoAdministradoresEvent extends AdministradoresEvent {}
 
 class DenegadoProfesoresEvent extends ProfesorEvent {}
 
+//evento de errores de las 3 variables
+
 class ErrorAlumnoEvent extends AlumnoEvent {}
 
 class ErrorAdministradoresEvent extends AdministradoresEvent {}
 
 class ErrorProfesorEvent extends ProfesorEvent {}
 
-//CrearCuenta
+//------ Evento de registrar una CrearCuenta--------------
 class LoginEvent {}
 
+//------ acepta la cuenta
 class AceptadaEvent extends LoginEvent {}
 
+//------ existe la cuenta
 class ExisteEvent extends LoginEvent {}
