a8da2ddf462ee199c0d27b6f8992d8ecd8e79772
Julian-Alcaraz <julianalcaraz4@gmail.com>
2021-11-20 15:14:42 -0300

    casi terminados los detalles antes de presentar

--- lib/UI/src/pages/administrativos/cargarCurso.dart
+++ lib/UI/src/pages/administrativos/cargarCurso.dart
@@ -1,3 +1,4 @@
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -132,6 +133,14 @@ class _CargarCursosState extends State<CargarCursos> {
                               crearCurso();
                               anio.clear();
                               division.clear();
+                              Fluttertoast.showToast(
+                                  msg: "¡Cargo el curso Exitosamente!",
+                                  toastLength: Toast.LENGTH_SHORT,
+                                  gravity: ToastGravity.BOTTOM,
+                                  timeInSecForIosWeb: 1,
+                                  backgroundColor: Colors.red,
+                                  textColor: Colors.white,
+                                  fontSize: 16.0);
                             } else {
                               print("erro");
                             }
--- lib/UI/src/pages/administrativos/cargarDatosProfesor.dart
+++ lib/UI/src/pages/administrativos/cargarDatosProfesor.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:schollinorder/UI/src/pages/login/event_google.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class CargarDatosProfesor extends StatefulWidget {
   @override
@@ -212,6 +213,15 @@ class _CargarDatosProfesorState extends State<CargarDatosProfesor> {
                             dni.clear();
                             materia1.clear();
                             materia2.clear();
+                            Fluttertoast.showToast(
+                                toastLength: Toast.LENGTH_SHORT,
+                                msg:
+                                    "¡Cargo el Profesor Exitosamente! \n Para ver los datos vaya a la pagina anterior",
+                                gravity: ToastGravity.CENTER,
+                                timeInSecForIosWeb: 1,
+                                backgroundColor: Colors.red,
+                                textColor: Colors.white,
+                                fontSize: 16.0);
                           } else {
                             print("erro");
                           }
